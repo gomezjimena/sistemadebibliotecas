85f577b9004c3218fad7bbab1f02cc5429caeea1
Gómezjimena <jimena.munoz@udea.edu.co>
2025-05-30 14:15:23 -0500

    verificacion de todo

--- udeainn/src/pages/login/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Login from '@/components/login/login-form';
-
-const Index = () => {
-  return <Login />;
-};
-
-export default Index;
--- udeainn/src/pages/usuarios/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Index = () => {
-  return <div>Hola Usuarios</div>;
-};
-
-export default Index;
