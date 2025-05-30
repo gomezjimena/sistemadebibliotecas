fb398b2912a4e393661c813accd0e5fb2c717c57
Gómezjimena <jimena.munoz@udea.edu.co>
2025-05-30 15:56:33 -0500

    Login

--- sistemabiblioteca/src/pages/_app.tsx
+++ sistemabiblioteca/src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Organisms/Layout';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
+  const router = useRouter();
+  const isLoginPage = router.pathname === '/login';
+
+  return isLoginPage ? (
+    <Component {...pageProps} />
+  ) : (
     <Layout>
       <Component {...pageProps} />
     </Layout>
