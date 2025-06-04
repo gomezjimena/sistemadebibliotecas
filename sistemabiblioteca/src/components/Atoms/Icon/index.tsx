5e5796e1425771fca38e1fec22a76d42f9b61da9
leonardo saenz <leonardo.saenz@udea.edu.co>
2025-06-03 23:15:19 -0500

    pull req

--- sistemabiblioteca/src/components/Atoms/Icon/index.tsx
+++ sistemabiblioteca/src/components/Atoms/Icon/index.tsx
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/react';
 const Index = ({ icon }: { icon: string }) => {
   return (
     <div className='flex flex-col items-center justify-center gradient w-[72px] h-[72px] rounded-full'>
-      <Icon icon={`${icon}`} width='40' height='40' className='text-white' />
+      <Icon icon={`${icon}`} width='90' height='90' className='text-black' />
     </div>
   );
 };
